e7bbb79340d1a5fe65dfe6653c36c78212051513
cucpk <98456717+cucpk@users.noreply.github.com>
2022-08-14 23:22:37 +0700

    Bai9

--- src/views/listProducts.vue
+++ src/views/listProducts.vue
@@ -1,9 +1,16 @@
 <template>
 <div>
-    <el-button type="success" round style="margin-top:50px">Thêm sản phẩm</el-button>
-    <el-button type="success" round>Sửa sản phẩm</el-button>
-    <el-button type="success" round style="margin-right:20px">Xóa sản phẩm</el-button>
-    <el-input placeholder="Tìm kiếm theo tên sản phẩm" v-model="input" style="width:250px"></el-input>
+    <el-button type="success" style="margin-right:10px" @click="openDialogCreateProduct()">Thêm mới</el-button>
+    <!-- // <el-button type="success" round>Sửa sản phẩm</el-button> -->
+    <!-- <el-button type="success" round style="margin-right:20px">Xóa sản phẩm</el-button> -->
+    <el-input style="width:250px"
+        placeholder="Nhập tên để tìm kiếm"
+        @clear="clearHandle()"
+        @keydown.enter.native="handleSearch()"
+          clearable
+        v-model="search">
+        <i slot="prefix" class="el-input__icon el-icon-search"></i>
+      </el-input>
     <p style="margin-top:50px">DANH SÁCH SẢN PHẨM</p>
     <el-table
       :data="products"
@@ -31,6 +38,35 @@
         label="image">
       </el-table-column>
     </el-table>
+    <el-dialog width="30%" top="5vh" v-loading="loading" title="Tạo sản phẩm" :visible.sync="dialogCreateProduct" class="modalUser">
+        <el-row>
+            <div class="inputWarp" >
+              <label>Tên sản phẩm </label>
+              <el-input v-model="name"></el-input>
+              <div class="error">
+                <span>{{errorName}}</span>
+              </div>
+            </div>
+            <div class="inputWarp" >
+              <label>Giá sản phẩm </label>
+              <el-input v-model="price"></el-input>
+              <div class="error">
+                <span>{{errorPrice}}</span>
+              </div>
+            </div>
+            <div class="inputWarp" >
+              <label>Mô tả </label>
+              <el-input type="textarea" :rows="5" v-model="description"></el-input>
+              <div class="error">
+                <span>{{errorDescription}}</span>
+              </div>
+            </div>
+          </el-row>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="closeDialogCreateProduct()">Đóng</el-button>
+        <el-button type="primary" @click="handleCreateProduct()">Tạo mới</el-button>
+      </span>
+    </el-dialog>
 </div>
 </template>
 <script>
@@ -67,6 +103,25 @@
       // }).catch((error) => {
       //   console.log(error);
       // });
+    },
+
+    method:{
+      openDialogCreateProduct(){
+          this.dialogCreateProduct = true
+          this.resetForm()
+          this.resetError()
+        },
+      handleSearch(){
+          if(this.search.length>0){
+            let params = {
+              q : this.search
+            }
+            this.getProduct(params)
+          }
+        },
+        clearHandle(){
+          this.getProduct()
+        },
     }
   }
 </script>
